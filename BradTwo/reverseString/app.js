da80ecc722e53b9a7e536b27e4176cff6fcf5888
‘Juliano <julianoperins@gmail.com>
2021-03-04 20:13:49 -0500

    reverse a string

---
+++ BradTwo/reverseString/app.js
@@ -0,0 +1,22 @@
+function reverseArray(str) {
+  //! Reverse
+  // const strArr = str.split("").reverse().join("");
+  // return strArr;
+  //! for loop decrement
+  // let revString = "";
+  // for (let i = str.length - 1; i >= 0; i--) {
+  // return revString;
+  //   //! -1 because the 0 index
+  //   revString = revString + str[i]; //! str[i] can be use in arrays and in strings
+  // }
+  // return revString;
+  //! for loop increment
+  // let revString = "";
+  // for (let i = 0; i <= str.length - 1; i++) {
+  //   revString = str[i] + revString;
+  //   console.log(str[i]);
+  // }
+  // return revString;
+}
+
+console.log(reverseArray("hello"));
--- Pedro/palindrome/app.js
+++
@@ -1,5 +0,0 @@
-function reverseArray(str) {
-  return str.split("").reverse().join("");
-}
-
-console.log(reverseArray("hello"));
