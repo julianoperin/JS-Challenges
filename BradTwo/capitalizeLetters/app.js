87c3f31dafad737e07fb643fd08c83c02d7fb6b3
‘Juliano <julianoperins@gmail.com>
2021-03-10 18:08:33 -0500

    capitalize each word in an array

---
+++ BradTwo/capitalizeLetters/app.js
@@ -0,0 +1,22 @@
+function capitalizeLetter(str) {
+  //! using for loop
+  // const newArr = str.toLowerCase().split(" ");
+
+  // for (let i = 0; i < newArr.length; i++) {
+  //   newArr[i] =
+  //     newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
+  // }
+
+  // return newArr.join(" ");
+
+  //! Using Map
+  const newStr = str
+    .toLowerCase()
+    .split(" ")
+    .map((item) => {
+      return item.substring(0, 1).toUpperCase() + item.substring(1);
+    });
+  return newStr.join(" ");
+}
+
+console.log(capitalizeLetter("i love pizza"));
