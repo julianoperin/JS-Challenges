78fff76574ae491aef15a559e424a610f5d19c4e
‘Juliano <julianoperins@gmail.com>
2021-03-10 14:24:52 -0500

    reverse integer

--- BradTwo/reverseString/app.js
+++ BradTwo/reverseString/app.js
@@ -1,44 +1,13 @@
-function reverseArray(str) {
-  //! Reverse
-  // const strArr = str.split("").reverse().join("");
-  // return strArr;
-  //! for loop decrement
-  // let revString = "";
-  // for (let i = str.length - 1; i >= 0; i--) {
-  // return revString;
-  //   //! -1 because the 0 index
-  //   revString = revString + str[i]; //! str[i] can be use in arrays and in strings
-  // }
-  // return revString;
-  //! for loop increment
-  // let revString = "";
-  // for (let i = 0; i <= str.length - 1; i++) {
-  //   revString = str[i] + revString;
-  //   console.log(str[i]);
-  // }
-  // return revString;
-  //! For of loop
-  //   let revString = "";
-  //   for (const char of str) {
-  //     revString = revString + char;
-  //   }
-  //   return revString;
+//!Reverse an Integer
 
-  //! ForEach
-  // revString = "";
-  // str.split("").forEach((item) => {
-  //   revString = item + revString;
-  // });
-  // return revString;
+function reverseInt(int) {
+  //!Reverse an integer
+  const newInt = int.toString().split("").reverse().join("");
+  return parseInt(newInt);
 
-  //! Map
-  // let revString = "";
-  // str.split("").map((item) => (revString = item + revString));
-  // return revString;
-
-  //! Reduce
-  return str.split("").reduce((accumulator, currentValue) => {
-    return currentValue + accumulator;
-  }, "");
+  //!Reverse an integer with negative value
+  // const newInt = int.toString().split("").reverse().join("");
+  // return parseInt(newInt) * Math.sign(int);
 }
-console.log(reverseArray("hello"));
+
+console.log(reverseInt(12345));
