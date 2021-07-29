90fd25f72d0c39a34274923373533d3455bfcb0b
‘Juliano <julianoperins@gmail.com>
2021-07-28 21:23:08 -0400

    Started js cardio 4

--- jsCardio4/add all/app.js
+++ jsCardio4/add all/app.js
@@ -13,14 +13,8 @@
 // console.log(addAllNumbers(2, 5, 6, 7));
 
 //! ES6
-function seekAndDestroy(arr, ...rest) {
-  for (let i = 0; i < rest.length; i++) {
-    for (let j = 0; i < arr.length; j++) {}
-    console.log(i);
-  }
-
-  console.log(arr);
-  console.log(rest);
+function addAllNumbers(...numbers) {
+  return numbers.reduce((acc, cV) => cV + acc, 0);
 }
 
-console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6));
+console.log(addAllNumbers(2, 5, 6, 7));
---
+++ jsCardio4/sum all primes/app.js
@@ -0,0 +1,9 @@
+//! Roman Numbers
+
+  //! Solution 1: 
+function sumAllPrimes(num) {
+  
+return
+}
+
+console.log(sumAllPrimes(10));
