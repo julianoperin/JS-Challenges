31a118108b8a5af7cce99d3ceebadefcf067feee
‘Juliano <julianoperins@gmail.com>
2021-08-05 22:05:45 -0400

    completed one more challenge

--- Recap/Palindrome/app.js
+++ Recap/Palindrome/app.js
@@ -1,44 +1,51 @@
-function isPalindrome(str) {
-  //!reverse
-  // const revString = str.split("").reverse().join("");
-  // return revString === str ? "It is Palindrome" : "It is not Palindrome";
-  //! for loop decrement
-  // let revString = "";
-  // for (let i = str.length - 1; i >= 0; i--) {
-  //   revString = revString + str[i];
-  // }
-  // if (str === revString) return "It is palindrome";
-  // if (str !== revString) return "It is NOT palindrome";
-  //! for loop increment
-  // let revString = "";
-  // for (let i = 0; i <= str.length - 1; i++) {
-  //   revString = str[i] + revString;
-  // }
-  // if (str === revString) return "It is palindrome";
-  // if (str !== revString) return "It is NOT palindrome";
-  //! for of
-  // let revString = "";
-  // for (let char of str) {
-  //   revString = char + revString;
-  // }
-  // return revString === str ? "It is Palindrome" : "It is not Palindrome";
-  //! forEach
-  // let revString = "";
-  // str.split("").forEach((item) => {
-  //   revString = item + revString;
-  // });
-  // return revString === str ? "It is Palindrome" : "It is not Palindrome";
-  //! map
-  // let revString = "";
-  // str.split("").map((item) => {
-  //   revString = item + revString;
-  // });
-  // return revString === str ? "It is Palindrome" : "It is not Palindrome";
-  //! reduce
-  const newReduceArr = str.split("").reduce((acc, currentValue) => {
-    return currentValue + acc;
-  }, "");
-  return newReduceArr === str ? "Palindrome" : "NOT Palindrome";
-}
+// function isPalindrome(str) {
+//!reverse
+// const revString = str.split("").reverse().join("");
+// return revString === str ? "It is Palindrome" : "It is not Palindrome";
+//! for loop decrement
+// let revString = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//   revString = revString + str[i];
+// }
+// if (str === revString) return "It is palindrome";
+// if (str !== revString) return "It is NOT palindrome";
+//! for loop increment
+// let revString = "";
+// for (let i = 0; i <= str.length - 1; i++) {
+//   revString = str[i] + revString;
+// }
+// if (str === revString) return "It is palindrome";
+// if (str !== revString) return "It is NOT palindrome";
+//! for of
+// let revString = "";
+// for (let char of str) {
+//   revString = char + revString;
+// }
+// return revString === str ? "It is Palindrome" : "It is not Palindrome";
+//! forEach
+// let revString = "";
+// str.split("").forEach((item) => {
+//   revString = item + revString;
+// });
+// return revString === str ? "It is Palindrome" : "It is not Palindrome";
+//! map
+// let revString = "";
+// str.split("").map((item) => {
+//   revString = item + revString;
+// });
+// return revString === str ? "It is Palindrome" : "It is not Palindrome";
+//! reduce
+//   const newReduceArr = str.split("").reduce((acc, currentValue) => {
+//     return currentValue + acc;
+//   }, "");
+//   return newReduceArr === str ? "Palindrome" : "NOT Palindrome";
+// }
 
+function isPalindrome(str) {
+  const reversed = str.split("").reduce((acc, cValue) => {
+    return cValue + acc;
+  }, "");
+
+  return str === reversed;
+}
 console.log(isPalindrome("racecar"));
