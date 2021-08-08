a9e4f44b940a5b0cc9de164a0e9bc45e080da68c
‘Juliano <julianoperins@gmail.com>
2021-08-07 23:19:16 -0400

    Finished  the uppercase challenge

--- Recap/capitalizeLetters/app.js
+++ Recap/capitalizeLetters/app.js
@@ -1,22 +1,33 @@
+// function capitalizeLetter(str) {
+//   //! using for loop
+//   // const newArr = str.toLowerCase().split(" ");
+
+//   // for (let i = 0; i < newArr.length; i++) {
+//   //   newArr[i] =
+//   //     newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
+//   // }
+
+//   // return newArr.join(" ");
+
+//   //! Using Map
+//   return str
+//     .toLowerCase()
+//     .split(" ")
+//     .map((item) => {
+//       return item.substring(0, 1).toUpperCase() + item.substring(1);
+//     })
+//     .join(" ");
+// }
+
+// console.log(capitalizeLetter("i love pizza"));
+
 function capitalizeLetter(str) {
-  //! using for loop
-  // const newArr = str.toLowerCase().split(" ");
+  const newArr = [];
+  str.split(" ").forEach((item) => {
+    newArr.push(item[0].toUpperCase() + item.slice(1));
+  });
 
-  // for (let i = 0; i < newArr.length; i++) {
-  //   newArr[i] =
-  //     newArr[i].substring(0, 1).toUpperCase() + newArr[i].substring(1);
-  // }
-
-  // return newArr.join(" ");
-
-  //! Using Map
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map((item) => {
-      return item.substring(0, 1).toUpperCase() + item.substring(1);
-    })
-    .join(" ");
+  return newArr.join(" ");
 }
 
 console.log(capitalizeLetter("i love pizza"));
