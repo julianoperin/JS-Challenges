4c117084c3bb1c04e7c65c8b07ff95d8b66c9dee
‘Juliano <julianoperins@gmail.com>
2021-08-13 23:40:14 -0400

    Solved Fibonacci challenge

--- SG/fib/index.js
+++ SG/fib/index.js
@@ -1,13 +1,11 @@
-// --- Directions
-// Print out the n-th entry in the fibonacci series.
-// The fibonacci series is an ordering of numbers where
-// each number is the sum of the preceeding two.
-// For example, the sequence
-//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-// forms the first ten entries of the fibonacci series.
-// Example:
-//   fib(4) === 3
+var i;
+var fib = []; // Initialize array!
 
-function fib(n) {}
-
-module.exports = fib;
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
