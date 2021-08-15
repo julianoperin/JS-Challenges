dfb4f9b19267fb0efea32f339e9e1c6c74a8d99a
‘Juliano <julianoperins@gmail.com>
2021-08-14 23:55:05 -0400

    Completed challenge missingLetter

---
+++ Recap/missingLetter/app.js
@@ -0,0 +1,15 @@
+function missingLetter(str) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  const chars = str.split("");
+
+  for (let i = 0; i <= chars.length; i++) {
+    if (chars[i] !== alphabet[i]) {
+      return alphabet[i];
+    }
+  }
+
+  return;
+}
+
+console.log(missingLetter("abcdef"));
